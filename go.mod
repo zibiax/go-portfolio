e9c18e340c147bce2a8265bd3791bec5e0090e1a
Martin <martin.evenbom@gmail.com>
2024-08-29 17:58:27 +0200

    implementing templ

--- go.mod
+++ go.mod
@@ -3,13 +3,17 @@ module go-portfolio
 go 1.23.0
 
 require (
+	github.com/a-h/templ v0.2.771
+	github.com/gin-gonic/gin v1.10.0
+)
+
+require (
 	github.com/bytedance/sonic v1.12.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
--- main.go
+++ main.go
@@ -1,36 +1,65 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
     "net/http"
+    "bytes"
+
+    "github.com/a-h/templ"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
     r := gin.Default()
+    templateEngine, err := templ.New()
+    if err != nil {
+        panic(err) // Handle initialization error
+    }
 
     // Serve the main page
     r.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", nil)
+        var buf bytes.Buffer
+        err := templateEngine.RenderTemplate(&buf, "templates/index.templ", gin.H{
+            "title": "Home Page",
+        })
+        if err != nil {
+            c.AbortWithError(http.StatusInternalServerError, err)
+            return
+        }
+        c.Data(http.StatusOK, "text/html; charset=utf-8", buf.Bytes())
     })
 
     // HTMX endpoints
-    r.GET("/about", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "about.html", nil)
-    })
-    r.GET("/projects", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "projects.html", nil)
-    })
-    r.GET("/contact", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "contact.html", nil)
-    })
+    setupHTMXEndpoints(r, templateEngine)
 
-    // Serve robots.txt
+    // Serve robots.txt and static assets
     r.StaticFile("/robots.txt", "./robots.txt")
-
-    // Static assets like CSS and JavaScript
     r.Static("/assets", "./assets")
 
     // Run the server
     r.Run(":8080")
 }
+
+func setupHTMXEndpoints(r *gin.Engine, templateEngine *templ.Template) {
+    // endpoints to template file
+    endpoints := map[string]string{
+        "/about": "templates/about.templ",
+        "/projects": "templates/projects.templ",
+        "/contact": "templates/contact.templ",
+    }
+
+    for path, tmpl := range endpoints {
+        r.GET(path, func(c *gin.Context) {
+            var buf bytes.Buffer
+            err := templateEngine.RenderTemplate(&buf, tmpl, gin.H{
+                "title": c.Param("title"),
+            })
+            if err != nil {
+                c.AbortWithError(http.StatusInternalServerError, err)
+                return
+            }
+            c.Data(http.StatusOK, "text/html; charset=utf-8", buf.Bytes())
+        })
+    }
+}
+
 
